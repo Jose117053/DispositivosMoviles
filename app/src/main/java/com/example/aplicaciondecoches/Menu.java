7d16d86fa930cce8df5d3646a1415874c09f60d8
Jose117053 <adolfo19420@gmail.com>
2024-10-24 23:59:26 -0600

    Uso de stringsxml y barra deslizable

--- app/src/main/java/com/example/aplicaciondecoches/Menu.java
+++ app/src/main/java/com/example/aplicaciondecoches/Menu.java
@@ -1,24 +1,46 @@
 package com.example.aplicaciondecoches;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class Menu extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerToggle;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_deslizable);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        buttonDrawerToggle =  findViewById(R.id.buttonDrawerToggle);
+
+
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
     }
+
+
+
 }
